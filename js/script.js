71e4b5e58a6a6a85f65d14e24eafa0c6324bf10c
Louise <Louise.blanc@chasacademy.se>
2023-12-05 21:41:49 +0100

    Update test branch html and JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,83 @@
-// Globala variabler
+const hangmanImage = document.querySelector("#hangman-image");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const letterButtons = document.querySelectorAll(".letterButtons");
+const messageBox = document.querySelector("#message");
+const startBtn = document.querySelector("#startBtn");
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+//counts wrong guesses, max 6 and shows player
+let currentWord, correctLetters = [], wrongGuessCount = 0;
+const maxGuesses = 6;
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+//getting words and hints from word-list.js
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word);
+    document.querySelector(".hint-text b").innerText = hint; //manipulates the dom and writes a hint
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join(""); //split separates the word into array, map goes through array create where letter is guessed, join puts it togheter again
+}
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+//gameover, can I move this? where is isVictory declared?
+const gameOver = (isVictory) => {
+    if (isVictory) {
+        alert("You won!");
+    } else {
+        alert("You lost!");
+    }
 
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
-// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+    setTimeout(() => {
+    }, 300); //3 sek timeout
+}
+
+//checks if clicked letters are correct and pushes them to letter-space
+const keyboardClick = (button, clickedLetter) => {
+    let lowerCaseLetter = clickedLetter.toLowerCase();
+    //checks if clickedLetter is exist on the currentWord
+    if (currentWord.includes(lowerCaseLetter)) {
+        //showing all correct letters on the word display
+        [...currentWord].forEach((letter, index) => {
+            console.log(...currentWord);
+            if (letter === lowerCaseLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+
+        //the images get showed in specific order if wrong guess
+    } else {
+        wrongGuessCount++;
+        hangmanImage.src = "images/h" + wrongGuessCount + ".png";
+    }
+
+    //disable clicked letters
+    button.disabled = true;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    //if(gameOver or isVictory) true = button.enable;
+
+    //checks if you guesses wrong more than 6 times then gameover
+    if (wrongGuessCount === maxGuesses) gameOver(false);
+    if (correctLetters.length === currentWord.lenght) gameOver(true);
+
+}
+
+//decides what happens when letter in keyboard is clicked
+const initButtons = () => {
+    letterButtons.forEach((letter) => {
+        letter.addEventListener("click", e => { keyboardClick(letter, e.target.value) });
+    })
+}
+
+const startGame = () => {
+    hangmanImage.src = "images/h0.png";
+    getRandomWord();
+    initButtons();
+}
+
+startBtn.addEventListener("click", startGame); //Last thing O-H helped with, not ready!!
+
+
+//Reset buttons
+//console.log correctletter.lenght
---
+++ word-list.js
@@ -0,0 +1,38 @@
+const wordList = [
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behaviour of the physical and natural world."
+    },
+    {
+        word: "twilight",
+        hint: "The daily hour before sun rises."
+    },
+    {
+        word: "coffeshop",
+        hint: "Where you go to get you caffeine."
+    },
+    {
+        word: "dance",
+        hint: "The rhytmic moving of your body to music."
+    },
+    {
+        word: "computer",
+        hint: "A machine that operates with ones and zeroes."
+    },
+    {
+        word: "developer",
+        hint: "A person who uses a computer to build stuff"
+    },
+    {
+        word: "oxygen",
+        hint: "Something you drag down you lungs everyday to survive"
+    },
+    {
+        word: "painting",
+        hint: "An art of using colors on paper or surface"
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of objects on the sky"
+    }
+]
